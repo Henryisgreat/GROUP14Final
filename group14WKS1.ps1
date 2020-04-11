66a013b5374d2fb156e2e79021a0c4bef98ddbab
Henryisgreat <54885134+Henryisgreat@users.noreply.github.com>
2020-04-11 13:14:04 -0400

    Add files via upload

---
+++ group14WKS1.ps1
@@ -0,0 +1,3 @@
+﻿New-NetIPAddress -InterfaceAlias Ethernet0 -IPAddress 10.0.5.100  -PrefixLength 24 -DefaultGateway 10.0.5.2
+Set-DnsClientServerAddress -InterfaceAlias "Ethernet0" -ServerAddresses 10.0.5.5
+Rename-Computer -NewName Group14
